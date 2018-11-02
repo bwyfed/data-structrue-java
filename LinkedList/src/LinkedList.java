2f5bb1732fda85462c8eaccf159898545b50dd6b
bwyfed <bianwy025@qq.com>
2018-11-02 16:42:27 +0800

    在链表中增加虚拟头结点，优化add操作。

--- LinkedList/src/LinkedList.java
+++ LinkedList/src/LinkedList.java
@@ -19,11 +19,11 @@ public class LinkedList<E> {
             return e.toString();
         }
     }
-    private Node head; // 头指针
-    int size; // 链表中元素的个数
+    private Node dummyHead; // 虚拟头指针
+    private int size; // 链表中元素的个数
 
     public LinkedList() {
-        head = null;
+        dummyHead = new Node(null, null);
         size = 0;
     }
     // 获取链表中的元素个数
@@ -34,19 +34,12 @@ public class LinkedList<E> {
     public boolean isEmpty() {
         return size == 0;
     }
-    // 在链表头添加新的元素e
-    public void addFirst(E e) {
-//        Node node = new Node(e);
-//        node.next = head;
-//        head = node;
-        head = new Node(e, head);
-        size++;
-    }
     // 在链表的index（0-based）位置添加新的元素e
     // 在链表中不是一个常用的操作，练习用:)
     public void add(int index, E e) {
         if(index < 0 || index > size)
             throw new IllegalArgumentException("Add failed. Illegal index.");
+        /*
         if(index == 0)
             addFirst(e);
         else {
@@ -59,10 +52,26 @@ public class LinkedList<E> {
             prev.next = new Node(e, prev.next);
             size++;
         }
+        */
+        Node prev = dummyHead;
+        for(int i = 0; i < index; i++)
+            prev = prev.next;
+
+        prev.next = new Node(e, prev.next);
+        size++;
     }
 
     // 在链表末尾添加新的元素e
     public void addLast(E e) {
         add(size, e);
+    }
+    // 在链表头添加新的元素e
+    public void addFirst(E e) {
+//        Node node = new Node(e);
+//        node.next = head;
+//        head = node;
+//        head = new Node(e, head);
+//        size++;
+        add(0, e);
     }
 }
