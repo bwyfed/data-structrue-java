c83df0a275e7451552d17854cf19b40e88632832
bwyfed <36172659@qq.com>
2019-11-24 09:52:56 +0800

    改变项目结构，每一节为一个新项目。

---
+++ 02-Array/01-Array-Basics/src/Main.java
@@ -0,0 +1,18 @@
+public class Main {
+    public static void main(String[] args) {
+        int[] arr = new int[10]; // 必须声明数组中有多少个元素
+        for(int i = 0; i < arr.length; i++)
+            arr[i] = i;
+
+        int[] scores = new int[]{100, 99, 66, 55};
+        for(int i = 0; i < scores.length; i++)
+            System.out.println((scores[i]));
+
+        scores[0] = 98;
+        for(int i = 0; i < scores.length; i++)
+            System.out.println((scores[i]));
+        // for each模式遍历数组
+//        for(int score:scores)
+//            System.out.println(score);
+    }
+}
