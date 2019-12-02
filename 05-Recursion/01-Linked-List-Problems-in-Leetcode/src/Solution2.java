c4ef0a6d7edfcd2d9964be395a9e5ddd66158ee8
Bianwy <bianwy025@qq.com>
2019-12-02 09:04:24 +0800

    实现删除单链表指定值的所有结点的功能。

---
+++ 05-Recursion/01-Linked-List-Problems-in-Leetcode/src/Solution2.java
@@ -0,0 +1,15 @@
+class Solution2 {
+    public ListNode removeElements(ListNode head, int val) {
+        ListNode dummyHead = new ListNode(-1); // 虚拟头结点
+        dummyHead.next = head;
+
+        ListNode prev = dummyHead;
+        while (prev.next != null) {
+            if (prev.next.val == val)
+                prev.next = prev.next.next;
+            else
+                prev = prev.next;
+        }
+        return dummyHead.next;
+    }
+}
