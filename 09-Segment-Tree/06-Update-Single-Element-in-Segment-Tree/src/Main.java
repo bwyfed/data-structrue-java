b74a0a43dea2ff5ec539a7284635ec529125c5af
bianwangyang <bianwy025@qq.com>
2023-01-24 10:53:53 +0800

    增加线段树的测试数据。

--- 09-Segment-Tree/06-Update-Single-Element-in-Segment-Tree/src/Main.java
+++ 09-Segment-Tree/06-Update-Single-Element-in-Segment-Tree/src/Main.java
@@ -1,6 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         Integer[] nums = {-2, 0, 3, -5, 2, -1};
+//        Integer[] nums = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
 //        SegmentTree<Integer> segTree = new SegmentTree<>(nums, new Merger<Integer>() {
 //            @Override
 //            public Integer merge(Integer a, Integer b) {
@@ -8,7 +9,7 @@ public class Main {
 //            }
 //        });
         SegmentTree<Integer> segTree = new SegmentTree<>(nums, (a, b) -> a + b);
-        // System.out.println(segTree);
+//        System.out.println(segTree);
 
         System.out.println(segTree.query(0,2));
         System.out.println(segTree.query(2,5));
