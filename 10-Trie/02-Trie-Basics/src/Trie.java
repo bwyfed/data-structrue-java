318d4e5d530e3027f55240c1831f270ef7c8d38a
bianwangyang <bianwy025@qq.com>
2023-01-24 12:16:47 +0800

    Trie字典树基础.

---
+++ 10-Trie/02-Trie-Basics/src/Trie.java
@@ -0,0 +1,44 @@
+import java.util.TreeMap;
+
+public class Trie {
+    private class Node{
+        public boolean isWord; // 找到这个node的时候，是否已经构成了一个单词
+        public TreeMap<Character, Node> next;
+
+        public Node(boolean isWord){
+            this.isWord = isWord;
+            next = new TreeMap<>();
+        }
+        public Node(){
+            this(false);
+        }
+    }
+    private Node root;
+    private int size;
+
+    public Trie(){
+        root = new Node();
+        size = 0;
+    }
+    // 获得Trie中存储的单词数量
+    public int getSize(){
+        return size;
+    }
+    // 向Trie中添加一个新的单词word。非递归实现
+    public void add(String word){
+        Node cur = root; // 初始的时候在root位置
+        for(int i = 0 ; i < word.length() ; i ++){
+            char c = word.charAt(i);
+            // 检查当前cur的next映射中是否已经包含了c到下一个节点的映射，没有的话则新建一个节点。
+            if(cur.next.get(c) == null)
+                cur.next.put(c, new Node());
+            // 有的话就走到下一个节点
+            cur = cur.next.get(c);
+        }
+
+        if(!cur.isWord){
+            cur.isWord = true;
+            size ++;
+        }
+    }
+}
