0332d72bd6087786b53328371698adec2b19dae7
bianwangyang <bianwy025@qq.com>
2023-01-25 13:15:13 +0800

    基于AVL树的集合和映射。

---
+++ 12-AVL-Tree/08-Map-and-Set-in-AVL-Tree/src/AVLSet.java
@@ -0,0 +1,33 @@
+public class AVLSet<E extends Comparable<E>> implements Set<E> {
+    // 对于集合来说，只需要有key即可，不对value做要求
+    private AVLTree<E, Object> avl;
+
+    public AVLSet(){
+        avl = new AVLTree<>();
+    }
+
+    @Override
+    public int getSize(){
+        return avl.getSize();
+    }
+
+    @Override
+    public boolean isEmpty(){
+        return avl.isEmpty();
+    }
+
+    @Override
+    public void add(E e){
+        avl.add(e, null);
+    }
+
+    @Override
+    public boolean contains(E e){
+        return avl.contains(e);
+    }
+
+    @Override
+    public void remove(E e){
+        avl.remove(e);
+    }
+}
